9d896ca4968259419d6a8e5452ad8f18ab1c5cca
Rudra <opgamerzxrudra@gmail.com>
2025-12-30 21:11:19 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../App.css";
+import "../../app.css";
 import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
