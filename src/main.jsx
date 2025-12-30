050685cf379f2616e08108bf0fef2e1456162d4e
Rudra <opgamerzxrudra@gmail.com>
2025-12-30 22:20:51 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import Login from "./pages/login";
-import Dashboard from "./pages/Dashboard";
-import "./index.css";
+import "./index.css"; // global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
